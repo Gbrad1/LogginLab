0005add06377965a4c0e84e59c8adeed4e08a5d0
Giles Bradford <gbradford@zipcodes-MacBook-Pro-4.local>
2020-02-05 12:45:28 -0500

    Added a test case to my test file.

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -31,4 +31,21 @@ public class LogginLabTest {
             }
         }
     }
+    @org.junit.Test
+    public void thresholdReached() {
+        Integer finalLimit = 10;
+
+        LogginLab lab = new LogginLab();
+        lab.setThreshold(finalLimit);
+
+        for (Integer i = 1; i <= finalLimit; i++) {
+            if (lab.thresholdReached(i)) {
+                logger.log(Level.INFO, "Threshold finally reached! ");
+                assertTrue(lab.thresholdReached(i));
+            } else {
+                logger.log(Level.INFO, "Threshold not reached Your number is: "+i);
+                assertFalse(lab.thresholdReached(i));
+            }
+        }
+    }
 }
